5c3b85f7fac4ed4c432b77da692ac291e4637206
enismermer <enismermer38@gmail.com>
2025-05-15 11:31:30 +0200

    Faire un lien entre frontend et backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [backendMessage, setBackendMessage] = useState("")
 
+  useEffect(() => {
+    fetch('https://backend-render-xjlv.onrender.com/')
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendMessage(data.message)
+        console.log(data)
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error)
+      })
+  }, []) // ← important : tableau vide = appel 1 seule fois au chargement
   return (
     <>
       <div>
@@ -21,6 +33,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <p>{backendMessage}</p>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
